f1456c73f96283953d642701727f3ab0bc7aa97e
MegWilliams17 <160405867+MegWilliams17@users.noreply.github.com>
2024-03-18 21:31:11 +0100

    Williams, E.S Python Hackathon

--- conditions.py
+++ conditions.py
@@ -9,3 +9,13 @@
 # - Prompts a user to enter their age.
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+
+
+#Promting a user to enter his/her age
+Age = int(input('Please Enter Your Age: '))
+
+#Using a conditional statement to determine if the user is eligible
+if Age >= 18:
+    print ("You are eligible to vote")
+else:
+    print ("You are not eligible to vote")
